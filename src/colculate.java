0300700e83180c1f31929de826874360d02d09c5
Greed <kanzew@outlook.com>
2017-04-07 23:35:02 +0500

    Course

---
+++ src/colculate.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Greed on 07.04.2017.
+ */
+public class colculate {
+    // first test git)
+    public static void main(String[] args) {
+        System.out.println("hi, gitHub");
+    }
+}
