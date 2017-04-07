125dfc053a4f93f1fd58df96ffe719eb6c8e4ac9
Greed <kanzew@outlook.com>
2017-04-08 00:02:28 +0500

    secindory lesson

--- src/colculate.java
+++ src/colculate.java
@@ -4,6 +4,9 @@
 public class colculate {
     // first test git)
     public static void main(String[] args) {
-        System.out.println("hi, gitHub");
+        System.out.println("Summ " + (Integer.valueOf(args[0]) + Integer.valueOf(args[1])));
+        System.out.println("Division " + (Integer.valueOf(args[0]) / Integer.valueOf(args[1])));
+        System.out.println("Multiplication " + (Integer.valueOf(args[0]) * Integer.valueOf(args[1])));
+        System.out.println("Difference " + (Integer.valueOf(args[0]) - Integer.valueOf(args[1])));
     }
 }
